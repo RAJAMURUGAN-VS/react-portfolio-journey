f7a3ea6a08fd67d9a61411e5baabab9ad25609a9
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2025-12-23 23:11:49 +0530

    feat: add auto-slide with timer reset on manual navigation

--- src/Components/HeroCarouselItem/HeroCarouselItem.jsx
+++ src/Components/HeroCarouselItem/HeroCarouselItem.jsx
@@ -5,21 +5,39 @@ import topProject from '../../data/topProjects.json'
 class HeroCarouselItem extends Component {
   state={index: 0}
 
+  onAutoSlide = () => {
+    this.intervalId=setInterval(this.onRightNavigation,4000)
+  }
+
+  componentDidMount() {
+    this.onAutoSlide()
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
+
   onLeftNavigation = () => {
+    clearInterval(this.intervalId)
+
     this.setState(prevState => ({
       index: (prevState.index===0)
-        ? prevState.index 
+        ? topProject.length-1
         : prevState.index-1
-      }
-    ))
+      }),
+      this.onAutoSlide
+    )
   }
 
   onRightNavigation = () => {
+    clearInterval(this.intervalId)
+
     this.setState(prevState => ({
       index: (prevState.index===topProject.length-1)
-        ? prevState.index
+        ? 0
         : prevState.index+1
-      })
+      }),
+      this.onAutoSlide
     )
   }
 
